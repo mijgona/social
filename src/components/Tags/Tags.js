51e95133f67fd069b8365781e7fe15fe29ba1fea
Mijgona <mijgon.9191@gmail.com>
2020-11-13 03:53:00 +0500

    add tags component

---
+++ src/components/Tags/Tags.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Tags({tags}) {
+       
+    return (
+               
+        <>
+        теги: {tags.map(o=> <button key={o}>#{o}</button>)}
+        </>
+    );
+}
+
+export default Tags
